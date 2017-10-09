9fb06f70f971e336deee1d443dd2ec8505e7a263
ramkdas12 <ramkdas12@gmail.com>
2017-10-09 18:23:49 +0530

    Adding Login page, headers and Footer

---
+++ bloodbank/src/app/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm, FormGroup, FormControl } from '@angular/forms';
+import { Validators, ValidatorFn } from '@angular/forms';
+import { AbstractControl } from '@angular/forms';
+@Component({
+  selector: 'bb-section',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  
+  login = {
+    email: '',
+    password: ''
+  }
+  pureEmail = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gi);
+  ngOnInit = function() {
+    
+  }
+  onSubmit = function(){
+    this.sbumitted = true;
+    console.log("FormSubmit");
+  }
+}
+
+function emailRegexValidator(nameRe: RegExp): ValidatorFn {
+  return (control: AbstractControl): {[key: string]: any} => {
+    const forbidden = nameRe.test(control.value);
+    console.log(forbidden);
+    return forbidden ? {'email': {value: control.value}} : null;
+  };
+}
+
